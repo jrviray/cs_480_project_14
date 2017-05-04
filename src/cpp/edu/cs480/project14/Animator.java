6e67e1e899004c78c28b4bbed8308f576415527b
Felix Zhang <ffzhang@cpp.edu>
2017-05-03 20:33:51 -0700

    Recommitted changes to code with ability to add nodes.

--- src/cpp/edu/cs480/project14/Animator.java
+++ src/cpp/edu/cs480/project14/Animator.java
@@ -1,8 +1,13 @@
 package cpp.edu.cs480.project14;
 
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 
 /**
@@ -16,23 +21,38 @@ public class Animator {
 
     private Edge[][] edgeTable;
 
-    double offsetX;
-    double offsetY;
+    private double offsetX;
+    private double offsetY;
 
+
+    /**
+     * This is a default constructor
+     * @param mainPane
+     */
     public Animator(Pane mainPane)
     {
         canvas=mainPane;
-        vertexTable = new Vertex[10];
-        edgeTable = new Edge[10][10];
+        vertexTable = new Vertex[2];
+        edgeTable = new Edge[2][2];
     }
 
-    public void createVertex(int vertexID, String context)
+    /**
+     * This method is used to create a new vertex on the canvas
+     * @param context
+     */
+    public void createVertex( String context)
     {
-        Vertex newVertex = new Vertex(vertexID,context);
-        if(vertexID==vertexTable.length) {
-            vertexTable = Arrays.copyOf(vertexTable, vertexTable.length * 2);
+        int ID;
+        for(ID=0;ID<vertexTable.length;ID++)
+        {
+            if(vertexTable[ID]==null)
+                break;
+        }
 
-            Edge[][] newEdgeTable = new Edge[edgeTable.length*2][edgeTable.length*2];
+        if(ID==vertexTable.length-1)    //expand the vertex table and edge table
+        {
+            vertexTable = Arrays.copyOf(vertexTable, vertexTable.length*2);
+            Edge[][] newEdgeTable = new Edge[vertexTable.length*2][vertexTable.length*2];
             for(int i=0;i<edgeTable.length;i++)
             {
                 for(int j=0;j<edgeTable.length;j++)
@@ -40,26 +60,74 @@ public class Animator {
                     newEdgeTable[i][j]=edgeTable[i][j];
                 }
             }
+            edgeTable=newEdgeTable;
         }
 
-        vertexTable[vertexID] = newVertex;
-        newVertex.setOnMousePressed(e -> {
-            offsetX = e.getSceneX() - newVertex.getX();
-            offsetY = e.getSceneY() - newVertex.getY();
-        });
+        //set the vertex to be able to drag around the canvas
+        Vertex newVertex = new Vertex(ID,context);
+        vertexTable[ID] = newVertex;
+        attachDragListener(newVertex);
+        drawOnCanvas(newVertex);
+    }
 
-        newVertex.setOnMouseDragged(e -> {
+    /**
+     * This method is used to attach a drag listener to a vertex
+     * @param vertex
+     */
+    private void attachDragListener(Vertex vertex)
+    {
+        vertex.setOnMousePressed(e -> {
+            offsetX = e.getSceneX() -vertex.getX();
+            offsetY = e.getSceneY() - vertex.getY();
+        });
+        vertex.setOnMouseDragged(e -> {
             double newX = e.getSceneX() - offsetX;
             double newY = e.getSceneY() - offsetY;
             if(newY>Vertex.RADIUS && newY<canvas.getBoundsInLocal().getHeight()-Vertex.RADIUS)
-                newVertex.setY(newY);
+                vertex.setY(newY);
             if(newX>Vertex.RADIUS && newX<canvas.getBoundsInLocal().getWidth()-Vertex.RADIUS)
-                newVertex.setX(newX);
+                vertex.setX(newX);
         });
-        drawOnCanvas(newVertex);
-
-
     }
+
+
+//    public void addEdge(int sourceID,int destID)
+//    {
+//        Edge newEdge = new Edge(vertexTable[sourceID],vertexTable[destID],15);
+//        drawOnCanvas(newEdge);
+//        newEdge.toBack();
+//    }
+
+    /**
+     * This method should be called whenever there is a change on vertices or edges
+     */
+    private void writeToFile()
+    {
+        try (PrintWriter writer = new PrintWriter("graph.txt", "UTF-8")) {
+
+            for(int i=0;i<edgeTable.length;i++)
+            {
+                for(int j=0;j<edgeTable.length;j++)
+                {
+                    if(edgeTable[i][j] !=null)
+                    {
+                        //write
+                    }
+                }
+            }
+
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        catch (UnsupportedEncodingException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
 
     private void drawOnCanvas(Node... elements)
     {
--- src/cpp/edu/cs480/project14/Edge.java
+++ src/cpp/edu/cs480/project14/Edge.java
@@ -1,16 +1,12 @@
 package cpp.edu.cs480.project14;
 
 import javafx.beans.binding.DoubleBinding;
-import javafx.beans.binding.ObjectBinding;
-import javafx.geometry.Point3D;
 import javafx.scene.Group;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Polygon;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.transform.Rotate;
-
-import java.io.InputStream;
 
 /**
  * Created by wxy03 on 4/23/2017.
@@ -25,8 +21,11 @@ public class Edge extends Group{
 
     private Text weightText;
 
-    public Edge(Vertex source,Vertex destination, int weight)
+    private double weight;
+
+    public Edge(Vertex source,Vertex destination, double weight)
     {
+        this.weight = weight;
         arrow = new Polygon();
 
         DoubleBinding lengthBinding = new DoubleBinding() {
@@ -94,7 +93,7 @@ public class Edge extends Group{
         edgeLine.endYProperty().bind(source.getYProperty());
         edgeLine.endXProperty().bind(source.getXProperty().add(Vertex.RADIUS).add(lengthBinding));
 
-        weightText = new Text(Integer.toString(weight));
+        weightText = new Text(Double.toString(weight));
         weightText.setFont(Font.font(15));
         weightText.translateXProperty().bind(source.getXProperty().add(lengthBinding.divide(2)));
         weightText.translateYProperty().bind(source.getYProperty().subtract(5));
@@ -106,9 +105,11 @@ public class Edge extends Group{
         this.getTransforms().add(rotate);
         getChildren().addAll(edgeLine,arrow,weightText);
 
-
     }
 
-
+public double getWeight()
+{
+    return weight;
+}
     
 }
